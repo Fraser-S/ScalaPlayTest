0296a4df3fe5bc4941fefc5ad0d260910d202a8a
Fraser Sprott <frasers@hotmail.co.uk>
2017-07-25 11:54:04 +0100

    added input routes and functions as well as cookie handling

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,8 +3,12 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.mvc.Http
 
 class Application extends Controller {
+
+  val numbers = (1 to 100)
+
   def index() = Action{
     Redirect("/home" )
   }
@@ -33,6 +37,56 @@ class Application extends Controller {
 
   def anyStatus() = Action{
     Status(488)("Strange Response Type")
+  }
+
+  def default(id :Long) = Action{
+    Ok(id.toString)
+  }
+
+  def dynamic(id :Long) = Action{
+    Ok(id.toString)
+  }
+
+  def set(input:String) = Action{
+    Ok(input)
+  }
+
+  def list(id: Int) = Action {
+    Ok(numbers(id).toString)
+  }
+
+  def getList() = Action {
+    Ok(numbers.toString)
+  }
+
+  def optional(id: Option[String]) = Action{
+    Ok(id.toString)
+  }
+
+  def reverseRoutingToHome() = Action {
+    Redirect(routes.Application.home())
+  }
+
+  def reverseRoutingTopageNotFound() = Action{
+    Redirect(routes.Application.pageNotFound())
+  }
+
+  def pageWithCookies() = Action{
+    Ok("This page uses cookies").withCookies(
+      Cookie("theme","blue"))
+  }
+
+  def editCookies() = Action{
+    Ok("Set to red").discardingCookies(DiscardingCookie("theme"))withCookies(
+      Cookie("theme","red"))
+  }
+
+  def discardCookies() = Action{
+    Ok("This page removed the cookie").discardingCookies(DiscardingCookie("theme"))
+  }
+
+  def displayCookie() = Action { request =>
+    request.cookies.get("theme").map{value => Ok(value.value)}.getOrElse(Ok("Cookie Jar Empty"))
   }
 }
 
